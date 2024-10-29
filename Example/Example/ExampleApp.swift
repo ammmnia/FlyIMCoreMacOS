c3308bb8a95bb65c3b4c59f226cfff826b62f9fa
joseph241006 <gualice939@gmail.com>
2024-10-29 15:40:05 +0400

    feat: example

---
+++ Example/Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Joseph on 29.10.24.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
